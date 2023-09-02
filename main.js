4e02725d4438ba7f98d6ddd46f6fcb670dbcae6c
TheeAlcoholicPROGMAMMER <etnaude11@gmail.com>
2023-09-02 18:15:10 +0100

    3rd

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 import './style.css'
 
 import * as THREE from 'three';
-
+// the scene/ camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+// the renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
-
+// the controls
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
-// what is this? I don't know it is the geometry of the sphere. Maybe
+// The object to be rendered
 const geometry = new THREE.SphereGeometry( 10, 32, 32 );
 const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add( torus );
+
+//Lights
 
 const pointLight = new THREE.PointLight(0xffffff, 100, 1000);
 pointLight.position.set(5,5,20)
@@ -28,7 +30,7 @@ const ambientLight = new THREE.AmbientLight( 0xffffff );
 scene.add( pointLight, ambientLight   );
 
 
-
+//Animation
 
 function animate() {
     requestAnimationFrame( animate );
